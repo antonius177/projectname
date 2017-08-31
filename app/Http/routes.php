9a94d062b45375b725d2e065e24e0de0aa228ac4
antonius177 <31312836+antonius177@users.noreply.github.com>
2017-08-31 20:19:01 +0700

    level2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,36 @@
 */
 
 Route::get('/', function () {
+    //return view('welcome');
+	return "Hi Guys...";
+});
+
+Route::get('/contoh', function () {
     return view('welcome');
 });
+
+Route::get('/admin/post', function () {
+    return ('admin is here.');
+});
+
+Route::get('/about', function () {
+    return ('hi about page.');
+});
+
+Route::get('/contact', function () {
+    return ('Hi, I am Contact.');
+});
+
+Route::get('/post/{id}', function ($id) {
+    return "This is Post number.  " . $id ;
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "This is Post number.  " . $id . " ". $name;
+});
+Route::get('/admin/post/example',  array('as'=>'admin.home',function () {
+    $url = route('admin.home');
+	return "This url is:  " . $url ;
+}));
+
+
